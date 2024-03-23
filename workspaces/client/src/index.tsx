f719d1348f2fe0de76deb2b5c3242885e5fb3aa6
hirokiwa <hirokifsys@gmail.com>
2024-03-24 04:43:11 +0900

    Chunk client index

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -4,16 +4,17 @@ import $ from 'jquery';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
-
-import { AdminApp } from '@wsh-2024/admin/src/index';
-import { ClientApp } from '@wsh-2024/app/src/index';
-
-import { preloadImages } from './utils/preloadImages';
 import { registerServiceWorker } from './utils/registerServiceWorker';
+import { lazy } from 'react';
+import React from 'react';
 
 const main = async () => {
   await registerServiceWorker();
   // await preloadImages();
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const ClientApp = lazy(() => import('@wsh-2024/app/src/index') as any);
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const AdminApp = lazy(() => import('@wsh-2024/admin/src/index') as any);
 
   $(document).ready(() => {
     if (window.location.pathname.startsWith('/admin')) {
@@ -22,9 +23,11 @@ const main = async () => {
       ReactDOM.hydrateRoot(
         $('#root').get(0)!,
         <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
-          <BrowserRouter>
-            <ClientApp />
-          </BrowserRouter>
+          <React.Suspense fallback="loading...">
+            <BrowserRouter>
+              <ClientApp />
+            </BrowserRouter>
+          </React.Suspense>
         </SWRConfig>,
       );
     }
